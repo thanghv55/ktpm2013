0d6681f3a373aefb8a654b14543ef211951ea623
thanghv55 <thanghv2011@gmail.com>
2013-10-18 03:51:30 +0700

    finish auto testcase and and validate

--- autotestcase/output.py
+++ autotestcase/output.py
@@ -5,44 +5,71 @@ Created on Oct 9, 2013
 '''
 import unittest
 import inspect
+import itertools
 from input import main
+
     
 doc = inspect.getdoc(main)
-print doc
 docs = doc.splitlines()
-    
-def split_variable(text):    
+
+#swap two number
+def swapTwoNumber(number1, number2):
+    temp = number2
+    number2 = number1
+    number1 = temp
+ 
+#sort two array have len equal
+def sortTwoArray(minList, maxList):
+    minList={}
+    maxList={}
+    for i in range(0,len(minList)):
+        if minList[i] > minList[i+1]:
+            swapTwoNumber(minList[i], minList[i+1])
+            swapTwoNumber(maxList[i], maxList[i+1])
+            
+#normalization string
+def normalization(text):    
     variable,words = text.split(":")
     words=words.strip()
     numbers = words.replace("][", ";")
     number2 = numbers.replace("]", "")
     number3 = number2.replace("[", "")
     number = number3.split(";")  
-    number1 = sorted(number, key=int)
     
-    Validate={}
-    i = 0 
-    k=0
-    while i<= len(number):
-        numb1 = number[i]
-        numb2 = number[i+1] 
-        Validate[k]= (numb1, numb2)
+    minList = {}
+    maxList = {}
+    k = 0
+    i = 0
+    while i < len(number) :
+        minList[k] = int(number[i])
+        maxList[k] = int(number[i+1])
         i = i+2
-    print Validate
-    
-    final_array = []  
-    b = int(0)
-    while True:
-        x = int(number1[b])
-        y = int(number1[b+1])
-        ans = (x + y)/2
-        final_array.append(ans)
-        b = b + 2
-        if b == len(number1):
-            break
-    return final_array
+        k = k+1
+    sortTwoArray(minList, maxList)
 
-def sum_array(L1,L2):
+    count = 0
+    for i in range(0, len(minList)-1):
+        if minList[i+1] <= maxList[i]:
+            count = count + 1
+    if count > 0:
+        raise Exception("input wrong! plsease try again!")
+    else:    
+        number1 = sorted(number, key=int)
+        final_array = []
+        b = int(0)
+        i = 0
+        while True:
+            x = int(number1[b])
+            y = int(number1[b+1])
+            ans = (x + y)/2
+            final_array.append(ans)
+            b = b + 2
+            if b == len(number1):
+                break
+        return final_array
+
+#combine cross two array 
+'''def mergeArray(L1,L2):
     length1=len(L1)
     length2=len(L2)
     b=[]
@@ -52,16 +79,16 @@ def sum_array(L1,L2):
                 str2 = L2[j] 
                 d = str(str1)+';'+ str(str2)
                 b.append(d)
-    return b
-def test_Case(docs):
-    bigArray=[]
-    for line in docs:
-        bigArray.append(split_variable(line))
+    return b'''
 
-    a=bigArray[0]
-    for k in range(1,len(bigArray)):
-        array_1 = bigArray[k]
-        a = sum_array(a, array_1)
+#creates test case from a list
+def testCase(docs):
+    bigArray=[]
+    i = 0
+    for line in docs:
+        bigArray.append(normalization(line))
+        i = i+1
+    a = list(itertools.product(*bigArray))
     return a
 
 class Test(unittest.TestCase):
@@ -72,20 +99,13 @@ def test_generator(a,b):
         self.assertEqual(a,b)
     return test
 
-result = test_Case(docs)
-result1={}
-j = 0
-for haiz in result:
-    result1[j] = haiz.split(';')
-    j = j+1
-print result1
+result = testCase(docs)
 
 i=0
-
 if __name__ == '__main__':
-    for t in result1:
+    for t in result:
         test_name = 'testName'+str(i)
-        test = test_generator(main(*result1[t]),"")
+        test = test_generator(main(*t),"tam giac deu")
         setattr(Test, test_name, test)
         i = i+1
     unittest.main()
